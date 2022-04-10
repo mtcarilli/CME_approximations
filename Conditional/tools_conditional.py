c983f917d39a200412dad88dcd98df3674c2cea3
Maria Carilli <miacarilli@gmail.com>
2022-04-10 11:26:22 -0700

    conditional scripts

---
+++ Conditional/tools_conditional.py
@@ -0,0 +1,202 @@
+import numpy as np
+import random 
+import matplotlib.pyplot as plt
+import torch
+import train_conditional as trc
+
+
+def plot_PMF_grid(data_list,npdf,nrows,ncols,model,get_ypred_at_RT,kld=True):
+    '''Plots predicted and true PMFs for random parameters chosen from file_list.
+    Number: nrows*ncols'''
+    
+    p_list,y_list = trc.unpack_data(data_list)
+    
+    rand = np.zeros(nrows*ncols)
+    
+    for i in range(nrows*ncols):
+        rand[i] = random.randint(0,len(y_list))
+    
+    y = []
+    Y = []
+    
+    for r in rand:
+        r = int(r)
+        y_pred = trc.get_predicted_PMF(p_list,position=r,model=model,get_ypred_at_RT=get_ypred_at_RT)
+        
+        y.append(y_list[r])
+        Y.append(y_pred)
+    
+    Y = [Y_.detach().numpy() for Y_ in Y]
+    y = [y_.detach().numpy() for y_ in y]
+    
+
+
+    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=(15, 15))
+    k = 0
+
+    for i in range(nrows):
+        for j in range(ncols):
+            
+            x = np.arange(len(y[k]))
+            axs[i,j].plot(x,y[k],'k-',label='True PMF')
+            axs[i,j].plot(x,Y[k],'r--',label='Predicted PMF')
+        
+            axs[i,j].set_xlabel('# mat RNA')
+            axs[i,j].set_ylabel('probability')
+
+            axs[i,j].legend()
+            
+            if kld == True:
+                kld_ = -np.sum(y[k]*np.log(Y[k]/y[k]))
+                axs[i,j].title.set_text(f'KLD: {kld_:.5f}')
+            k = k + 1
+
+        
+    fig.tight_layout()
+    
+def plot_PMF(p_list,y_list,model,npdf,get_ypred_at_RT,kld=True):
+    '''Plots predicted and true PMF for given parameter, ykerlist and ylist (one p, yker, y in each list)'''
+    
+    
+    y_pred = trc.get_predicted_PMF(p_list=p_list,
+                                y_list=y_list,npdf=npdf,position=0,model=model,get_ypred_at_RT = get_ypred_at_RT)
+    
+    
+    Y = y_pred.detach().numpy() 
+    y = y_list[0].detach().numpy()
+
+
+    x = np.arange(len(y))
+    plt.plot(x,y,'k-',label='True PMF')
+    plt.plot(x,Y,'r--',label='Predicted PMF')
+        
+    plt.xlabel('# mat RNA')
+    plt.ylabel('probability')
+
+    plt.legend()
+            
+    if kld == True:
+        kld_ = -np.sum(y*np.log(Y/y))
+        plt.title(f'KLD: {kld_:.5f}')
+    plt.show()
+
+
+def plot_CDF(array,metric='KLD'):
+    '''Plots CDF'''
+    cdf = np.zeros(len(array))
+    array_sorted = np.sort(array)
+    for i,value in enumerate(array_sorted):
+        cdf[i] = len(array_sorted[array_sorted<value])/len(array_sorted)
+
+    plt.scatter(array_sorted,cdf,s=5)
+    plt.title(f'{metric CDF}')
+    plt.xlabel(f'{metric}')
+    plt.ylabel('CDF')
+       
+    plt.show()
+
+
+def plot_histogram(array,bins,metric='kld'):
+    '''Histogram of bin number of bins, xlim'''
+    plt.hist(array,bins = bins)
+    plt.title(f'Average {metric}: {np.mean(array):.4f}')
+    plt.xlabel(f'{metric} Divergence')
+    plt.ylabel('Frequency')
+    
+def plot_training(e_,t_,metric='kld'):
+    '''Plots training data'''
+    plt.figure(figsize=(9,6))
+    plt.plot(range(len(e_)),e_,c='blue',label='Training Data')
+    plt.plot(range(len(t_)),t_,c='red',label='Testing Data')
+    plt.xlabel('Epoch')
+    plt.ylabel(f'{metric}')
+    plt.title('Loss vs. epoch')
+    plt.legend()
+    
+def get_parameters_quantile(train_list,model,klds,quantiles = [.95,1.0]):
+    '''Returns given percent parameters with the highest klds and klds.'''
+    
+    parameters,y_list = rt1.load_training_data(train_list)
+    
+    kld_low = np.quantile(klds,quantiles[0])
+    kld_high = np.quantile(klds,quantiles[1])
+    
+    klds_segment = klds[klds>kld_low]
+    params_segment = parameters[klds>kld_low]
+    
+    klds_segment_2 = klds_segment[klds_segment<kld_high]
+    params_segment_2 = params_segment[klds_segment<kld_high]
+    
+    return(params_segment_2,klds_segment_2)
+
+def plot_param_quantiles(klds,train_list,model):
+    
+    params_segment_1,klds_segment_1 = get_parameters_quantile(train_list,model,klds,quantiles=[0,.25])
+    params_segment_2,klds_segment_2 = get_parameters_quantile(train_list,model,klds,quantiles=[.25,.5])
+    params_segment_3,klds_segment_3 = get_parameters_quantile(train_list,model,klds,quantiles=[.5,.75])
+    params_segment_4,klds_segment_4 = get_parameters_quantile(train_list,model,klds,quantiles=[.75,.95])
+    params_segment_5,klds_segment_5 = get_parameters_quantile(train_list,model,klds,quantiles=[.95,1.])
+    
+    b_1 = 10**np.array([ p[0] for p in params_segment_1 ])
+    beta_1 = 10**np.array([ p[1] for p in params_segment_1  ])
+    gamma_1 = 10**np.array([ p[2] for p in params_segment_1  ])
+
+    b_2 = 10**np.array([ p[0] for p in params_segment_2 ])
+    beta_2 = 10**np.array([ p[1] for p in params_segment_2  ])
+    gamma_2 = 10**np.array([ p[2] for p in params_segment_2  ])
+
+    b_3 = 10**np.array([ p[0] for p in params_segment_3 ])
+    beta_3 = 10**np.array([ p[1] for p in params_segment_3  ])
+    gamma_3 = 10**np.array([ p[2] for p in params_segment_3  ])
+
+    b_4 = 10**np.array([ p[0] for p in params_segment_4 ])
+    beta_4 = 10**np.array([ p[1] for p in params_segment_4  ])
+    gamma_4 = 10**np.array([ p[2] for p in params_segment_4  ])
+
+    b_5 = 10**np.array([ p[0] for p in params_segment_5 ])
+    beta_5 = 10**np.array([ p[1] for p in params_segment_5  ])
+    gamma_5 = 10**np.array([ p[2] for p in params_segment_5  ])
+    
+    fig,ax = plt.subplots(nrows=1,ncols=3,figsize=(12,5))
+    
+    
+    # some labels
+    ax[0].scatter(10,10,c='grey',label = 'Quantile 0-0.25')
+    ax[0].scatter(10,10,c='blue',label = 'Quantile 0.25-0.50')
+    ax[0].scatter(10,10,c='purple',label = 'Quantile 0.50-0.75')
+    ax[0].scatter(10,10,c='green',label = 'Quantile 0.75-0.95')
+    ax[0].scatter(10,10,c='red',label = 'Quantile 0.95-1.0')
+
+    ax[0].scatter(b_1,beta_1,c = klds_segment_1, cmap= 'Greys')
+    ax[0].scatter(b_2,beta_2,c = klds_segment_2, cmap= 'Blues')
+    ax[0].scatter(b_3,beta_3, c = klds_segment_3, cmap= 'Purples')
+    ax[0].scatter(b_4,beta_4,c = klds_segment_4, cmap= 'Greens')
+    ax[0].scatter(b_5,beta_5,c = klds_segment_5, cmap= 'Reds')
+    ax[0].set_xlabel('b')
+    ax[0].set_ylabel('beta')
+    ax[0].set_xscale('log')
+    ax[0].set_yscale('log')
+    
+    ax[1].scatter(b_1,gamma_1,c = klds_segment_1, cmap= 'Greys')
+    ax[1].scatter(b_2,gamma_2, c = klds_segment_2, cmap= 'Blues')
+    ax[1].scatter(b_3,gamma_3,c = klds_segment_3, cmap= 'Purples')
+    ax[1].scatter(b_4,gamma_4,c = klds_segment_4, cmap= 'Greens')
+    ax[1].scatter(b_5,gamma_5,c = klds_segment_5, cmap= 'Reds')
+    ax[1].set_xlabel('b')
+    ax[1].set_ylabel('gamma')
+    ax[1].set_xscale('log')
+    ax[1].set_yscale('log')
+    
+    ax[2].scatter(beta_1,gamma_1,c = klds_segment_1, cmap= 'Greys')
+    ax[2].scatter(beta_2,gamma_2, c = klds_segment_2, cmap= 'Blues')
+    ax[2].scatter(beta_3,gamma_3,c = klds_segment_3, cmap= 'Purples')
+    ax[2].scatter(beta_4,gamma_4,c = klds_segment_4, cmap= 'Greens')
+    ax[2].scatter(beta_5,gamma_5,c = klds_segment_5, cmap= 'Reds')
+    ax[2].set_xlabel('beta')
+    ax[2].set_ylabel('gama')
+    ax[2].set_xscale('log')
+    ax[2].set_yscale('log')
+ 
+    ax[0].legend()
+    fig.tight_layout()
+    plt.title('MLP 1 Parameters Colored by KLD Quantile')
---
+++ Conditional/train_conditional.py
@@ -0,0 +1,303 @@
+# load in modules
+
+import random
+import numpy as np
+from itertools import repeat
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# get file paths
+def get_file_paths(set_size,number_of_training_files,number_of_testing_files,total_files,file_path):
+    train_list = []
+    test_list = []
+    
+    for i in range(number_of_training_files):
+        num = i
+        file_list_ = load_data_list(file_path+f'{set_size}_{i}.npy')
+        train_list = train_list + file_list_
+
+    for i in range(number_of_testing_files):
+        num = total_files-i-1
+        file_list_ = load_data_list(file_path+f'{set_size}_{i}.npy')
+        test_list = test_list + file_list_
+
+    return(train_list,test_list) 
+
+
+# load in data
+def load_data_list(file_path):
+    full_file_list = list(np.load(file_path,allow_pickle=True))
+    return(full_file_list)
+
+# unpack data
+def unpack_data(data_list):
+    '''Load .npy file, returns tensor for parameters and ground truth histograms'''
+ 
+    ps = np.array([ a[0] for a in data_list ])
+    p_tensor = torch.from_numpy(ps).float()
+    y_tensor = [ torch.tensor(a[1]).float() for a in data_list ]
+    
+    return(p_tensor,y_tensor)
+
+
+# shuffle data 
+def shuffle_data(data_list):
+    '''shuffles the pre-loaded data list (keeps param vectors with y values)'''
+    
+    random.shuffle(data_list)
+    parameters = np.array([ a[0] for a in data_list ])
+    parameters_tensor = torch.from_numpy(parameters).float()
+    y_tensor = [ torch.tensor(a[1]).float() for a in data_list ]
+    
+    return(parameters_tensor,y_tensor)
+
+# get moments
+def get_moments(p):
+    b,beta,gamma=p
+    
+    r = torch.tensor([1/beta, 1/gamma])
+    MU = b*r
+    VAR = MU*torch.tensor([1+b,1+b*beta/(beta+gamma)])
+    STD = torch.sqrt(VAR)
+    xmax = torch.ceil(MU)
+    xmax = torch.ceil(xmax + 4*STD)
+    xmax = torch.clip(xmax,30,np.inf).int()
+    return MU, VAR, STD, xmax
+
+
+
+
+# get metrics
+def get_metrics(ypred,y,metric):
+    '''Calculates desired metric between predicted probability and y.'''
+    
+
+    y = torch.flatten(y)
+    ypred = torch.flatten(ypred)
+
+    if metric=='kld':
+        return -torch.sum(y*torch.log(ypred/y))
+    if metric=='kld_normalized':
+        return -torch.sum(y*torch.log(ypred/y))/y.size(0)
+    if metric=='totalse':
+        return torch.sum((ypred-y)**2)
+    if metric=='mse':
+        return torch.mean((ypred-y)**2)
+    if metric=='maxabsdev':
+        return torch.max(torch.abs(ypred-y))
+    if metric=='maxabsdevlog':
+        return torch.max(torch.abs(torch.log(ypred)-torch.log(y)))
+    if metric=='mselog':
+        return torch.mean((torch.log(ypred)-torch.log(y))**2)
+
+
+# calculate test klds 
+def calculate_test_metrics(test_list,model,get_ypred_at_RT,metric):
+    model.eval()
+    p_list,y_list = unpack_data(test_list)
+    metrics = np.zeros(len(p_list))
+    
+    for i in range(len(p_list)):
+        y = y_list[i].flatten()
+
+        ypred = get_predicted_PMF(p_list,i,model,get_ypred_at_RT)
+        ypred = ypred.flatten()
+
+        metric_ = get_metrics(ypred,y,metric)
+        metrics[i] = metric_.detach().numpy()
+        
+    return(metrics,np.mean(metrics))
+
+# get predicted PMF
+def get_predicted_PMF(p_list,position,model,get_ypred_at_RT):
+    '''Returns predicted histogram for p given current state of model.'''
+    model.eval()
+
+    p_ = p_list[position:position+1]
+    w_,hyp_,w_un_ = model(p_)
+
+    p = p_[0]
+    w = w_[0]
+    hyp = hyp_[0]
+
+    ypred = get_ypred_at_RT(p,w,hyp)
+    
+    return ypred 
+
+
+# define loss function
+def loss_fn(ps,ys,w,hyp,get_ypred_at_RT,metric):
+    '''Calculates average metval over batch between predicted Y and y.
+    yker_list and y_list are actually lists of tensor histograms with first dimension batchsize'''
+    
+    metval = torch.tensor(0.0)
+    
+    batchsize = len(ps)
+ 
+
+    for b in range(batchsize):
+       
+        y_ = ys[b]
+        p_ = ps[b]
+        w_ = w[b]
+        hyp_ = hyp[b]
+        
+        ypred_ = get_ypred_at_RT(p_,w_,hyp_)
+        
+        met_ = get_metrics(ypred_,y_,metric)
+        
+        metval += met_
+        #print('training metval: ',metval)
+
+
+    return metval/batchsize
+
+# define model
+class my_MLP1(nn.Module):
+
+    def __init__(self, input_dim, output_dim, h1_dim, h2_dim, norm_type='softmax'):
+        super().__init__()
+
+        self.input = nn.Linear(input_dim, h1_dim)
+        self.hidden = nn.Linear(h1_dim, h2_dim)
+        self.output = nn.Linear(h2_dim, output_dim)
+
+        self.hyp = nn.Linear(h2_dim,1)
+
+        self.softmax = nn.Softmax(dim=1)
+        self.sigmoid = torch.sigmoid
+        
+        self.norm_type = norm_type
+        
+
+    def forward(self, inputs):
+
+        # pass inputs to first layer, apply sigmoid
+        l_1 = F.sigmoid(self.input(inputs))
+
+        # pass to second layer, apply sigmoid
+        l_2 = F.sigmoid(self.hidden(l_1))
+        
+        # pass inputs out (unnormalized)
+        w_un = (self.output(l_2))
+        
+        # pass out hyperparameter, sigmoid so it is between 0 and 1, then scale between 1 and 6
+        hyp = self.sigmoid(self.hyp(l_2))*5+1 
+    
+
+        # apply normalization or softmax
+        if self.norm_type == 'softmax':
+            w_pred = self.softmax(w_un)
+      
+        elif self.norm_type == 'normalize':
+            # before normalization, apply sigmoid to ensure all weights are positive
+            w_pred_ = F.sigmoid(w_un)
+            w_pred = (w_un/w_un.sum(axis=0)).sum(axis=0)
+
+        else:
+            # just take absolute value of output weights
+            w_pred = torch.abs(self.output(w_un)) #no softmax at all
+    
+
+        # return unnormalized weights in case you want to apply l1 regularization 
+        return w_pred, hyp, w_un 
+
+
+
+
+
+def run_epoch(p_list,y_list,model,optimizer,batchsize,get_ypred_at_RT,metric):
+
+    model.train()
+
+    # number of batches (data/batchsize)
+    trials = int(np.floor(p_list.size()[0] / batchsize ))
+
+    # should this be an array or a tensor ???? 
+    metvals = torch.zeros(trials)
+
+    for j in range(trials):
+        i = j * batchsize
+        ps = p_list[i:i+batchsize]
+        ys = y_list[i:i+batchsize]
+
+        # Zero the gradients
+        optimizer.zero_grad()
+        
+        # Perform forward pass
+        w, hyp, w_un  = model(ps)
+
+        # Compute loss
+    
+        loss = loss_fn(ps,ys,w,hyp,get_ypred_at_RT,metric)
+        
+        # average metric for the batch j
+        metvals[j] = loss.item()
+
+        # Perform backward pass
+        loss.backward()
+      
+        # Perform optimization
+        optimizer.step()
+    
+    # calculate the average metric over the epoch 
+    av_metval = torch.mean(metvals)
+
+    return(av_metval)
+
+
+def train(train_list,test_list,model_config,train_config,get_ypred_at_RT):
+    
+    train_list_ = train_list
+    # define model configurations
+    npdf = model_config['npdf']
+    h1_dim = model_config['h1_dim']
+    h2_dim = model_config['h2_dim']
+    norm_type = model_config['norm_type']
+    
+
+    # define model
+    model = my_MLP1(input_dim=4, output_dim=npdf,
+     h1_dim=h1_dim, h2_dim=h2_dim, norm_type=norm_type)
+        
+
+    # define training configurations
+    num_epochs = train_config['num_epochs']
+    lr = train_config['lr']
+    batchsize = train_config['batchsize']
+    metric = train_config['metric']
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr) 
+
+    
+    epoch_metvals = np.zeros(num_epochs)
+    test_metvals = np.zeros(num_epochs)
+    
+    for e in range(num_epochs):
+        print('Epoch Number:',e)
+
+     
+
+        batch_metrics_ = []
+
+        p_list,y_list = shuffle_data(train_list_)
+        
+
+        # REDEFINE
+        metval_ = run_epoch(p_list,y_list,model,optimizer,batchsize,get_ypred_at_RT,metric)
+       
+        # store epoch metric
+        epoch_metvals[e] = metval_
+
+
+        # test by evaluating the model
+        
+        test_metval_list_,test_metval_ = calculate_test_metrics(test_list,model,get_ypred_at_RT,metric)
+        
+        # store test metric
+        test_metvals[e] = test_metval_
+    
+
+    return(epoch_metvals, test_metvals, model)
